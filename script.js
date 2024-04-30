b09afcb084ee77202fc510262a58cbb7288c61cd
Patricia <patricia.abadaranda@gmail.com>
2024-04-30 13:51:50 +0200

    Responsive header actualizado.

--- script.js
+++ script.js
@@ -30,3 +30,33 @@ function closeModal(modal) {
     overlay.classList.remove('active');
 }
 
+/* ----- menu responsive -------*/
+//Variables
+const menu = document.querySelector(".menu-responsive");
+const menuItems = document.querySelectorAll(".menu-item");
+const hamburger= document.querySelector(".btn-hamburger");
+const closeIcon= document.querySelector(".close-icon");
+const menuIcon = document.querySelector(".menu-icon");
+
+// Funcion para mostrar el menu
+function toggleMenu() {
+  if (menu.classList.contains("show-menu")) {
+    menu.classList.remove("show-menu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+    document.body.classList.remove("menu-open");
+  } else {
+    menu.classList.add("show-menu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+    document.body.classList.add("menu-open");
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
